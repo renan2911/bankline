233ecc4bee155141e12b962d1608e79b7cce7d33
renan2911 <renanalysson2911@hotmail.com>
2021-02-26 23:14:58 -0300

    Criação do projeto

---
+++ src/main/java/br/com/gama/bankline/BanklineApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gama.bankline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gama/bankline/BanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gama.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
