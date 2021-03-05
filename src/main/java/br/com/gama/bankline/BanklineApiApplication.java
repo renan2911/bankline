b1e6e5c8a015ab0ae903c4b6cca81f79cbb2a63d
renan2911 <renanalysson2911@hotmail.com>
2021-03-05 10:02:20 -0300

    Realizando testes no Heroku

--- src/main/java/br/com/gama/bankline/BanklineApiApplication.java
+++ src/main/java/br/com/gama/bankline/BanklineApiApplication.java
@@ -3,14 +3,9 @@ package br.com.gama.bankline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnableSwagger2
 public class BanklineApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
 	}
-
 }
